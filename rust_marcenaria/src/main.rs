cccf69f23229d7a4a269db472459c2c03bf66c08
Gabriel M. Costa <gmessiasc@gmail.com>
2024-01-21 01:07:57 -0300

    feat: aa

--- rust_marcenaria/src/main.rs
+++ rust_marcenaria/src/main.rs
@@ -1,15 +1,17 @@
+#[global_allocator]
+static GLOBAL: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+
 mod routes;
 mod exceptions;
 mod models;
 mod utils;
 
-use ntex::{web::{self, App, route}, util::PoolId, http, time::Seconds};
-
-#[web::get("/")]
-async fn index() -> &'static str {
-  println!("Recebendo uma requisição... ");
-  "Hello world!"
-}
+use ntex::{
+  web::{self, route},
+  util::PoolId,
+  http,
+  time::Seconds,
+};
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
@@ -17,23 +19,34 @@ async fn main() -> std::io::Result<()> {
     .backlog(1024)
     .workers(num_cpus::get())
     .bind("marcenaria", "0.0.0.0:8080", |cfg| {
-            cfg.memory_pool(PoolId::P1);
-            PoolId::P1.set_read_params(65535, 2048);
-            PoolId::P1.set_write_params(65535, 2048);
+      cfg.memory_pool(PoolId::P1);
+      PoolId::P1.set_read_params(65535, 2048);
+      PoolId::P1.set_write_params(65535, 2048);
 
-            http::HttpService::build()
-                .keep_alive(http::KeepAlive::Os)
-                .client_timeout(Seconds::ZERO)
-                .headers_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
-                .payload_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
-                .h1(web::App::new()
-                        .configure(routes::openapi::ntex_config)
-                        .route("/materiais", web::get().to(routes::orcamento::get_materiais))
-                        .route("/geometrias", web::get().to(routes::orcamento::get_geometrias))
-                        .route("/orcamento", web::post().to(routes::orcamento::post_orcamento))
-                        .default_service(route().to(routes::default)))
-        })
-        .unwrap()
-  .run()
-  .await
+      http::HttpService::build()
+        .keep_alive(http::KeepAlive::Os)
+        .client_timeout(Seconds::ZERO)
+        .headers_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
+        .payload_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
+        .h1(
+          web::App::new()
+            .configure(routes::openapi::ntex_config)
+            .route(
+              "/materiais",
+              web::get().to(routes::orcamento::get_materiais),
+            )
+            .route(
+              "/geometrias",
+              web::get().to(routes::orcamento::get_geometrias),
+            )
+            .route(
+              "/orcamento",
+              web::post().to(routes::orcamento::post_orcamento),
+            )
+            .default_service(route().to(routes::default)),
+        )
+    })
+    .unwrap()
+    .run()
+    .await
 }
